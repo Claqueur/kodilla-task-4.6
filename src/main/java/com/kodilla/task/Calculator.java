756a671bfcab28e83cd33886f005ae244eeec22c
Lukasz Strycharz <l.strycharz.private@gmail.com>
2021-06-30 19:37:56 +0200

    Completing Task

--- src/main/java/com/kodilla/task/Calculator.java
+++ src/main/java/com/kodilla/task/Calculator.java
@@ -1,4 +1,13 @@
 package com.kodilla.task;
 
-public class Program {
+public class Calculator {
+    public Calculator(){
+
+    }
+    public Double addition(Double componentOne, Double componentTwo){
+        return componentOne + componentTwo;
+    }
+    public Double subtraction(Double minuend, Double subtrahend){
+        return minuend - subtrahend;
+    }
 }
--- src/main/java/com/kodilla/task/TaskApplication.java
+++ src/main/java/com/kodilla/task/TaskApplication.java
@@ -2,12 +2,19 @@ package com.kodilla.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.*;
 
 @SpringBootApplication
 public class TaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskApplication.class, args);
+        Calculator calculator = new Calculator();
+        Double numberA = 12.23;
+        Double numberB = 23.45;
+        System.out.println(calculator.addition(numberA, numberB));
+        System.out.println(calculator.subtraction(numberA, numberB));
+
     }
 
 }
